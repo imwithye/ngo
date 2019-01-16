ac5e745d52170336d84aefc88181eecb697637c7
Ciel <imwithye@gmail.com>
2019-01-16 12:36:12 +0800

    add comment

--- example/example.go
+++ example/example.go
@@ -5,12 +5,14 @@ import (
 	"strings"
 )
 
+//ToUpper function converts a string to upper case
 //export ToUpper
 func ToUpper(payload *C.char) *C.char {
 	str := strings.ToUpper(C.GoString(payload))
 	return C.CString(str)
 }
 
+//ToLower function converts a string to lower case
 //export ToLower
 func ToLower(payload *C.char) *C.char {
 	str := strings.ToLower(C.GoString(payload))
